71ebf33acbe61dc4aa00948d34632c402ec77669
Mehul Bhuva <mbhuva1@cs.binghamton.edu>
2018-10-03 23:45:58 -0400

    Tree builder delete and insert method added

--- mehul_bhuva_assign_2/studentCoursesBackup/src/studentCoursesBackup/myTree/Node.java
+++ mehul_bhuva_assign_2/studentCoursesBackup/src/studentCoursesBackup/myTree/Node.java
@@ -1,20 +1,21 @@
 package studentCoursesBackup.myTree;
+
 import java.util.ArrayList;
 
 import studentCoursesBackup.student.Student;
 
-public class Node implements ObserverI, SubjectI{
+public class Node implements ObserverI, SubjectI {
 	private int key;
 	private ArrayList<String> courseList = new ArrayList<String>();
 	private String Course;
-    private Node left, right; 
-    private Node ListnerBackup1;
-    private Node ListnerBackup2;
-    private String insert= "insert";
-    private String remove= "remove";
-    int originalTree = 0;
-    
-    public int getKey() {
+	private Node left, right;
+	private Node ListnerBackup1;
+	private Node ListnerBackup2;
+	private String insert = "insert";
+	private String remove = "remove";
+	int originalTree = 0;
+
+	public int getKey() {
 		return key;
 	}
 
@@ -38,15 +39,77 @@ public class Node implements ObserverI, SubjectI{
 		this.Course = newCourse;
 	}
 
-    
-    
-    public Node(int bnumber, String course) 
-    { 
-        this.key = bnumber; 
-        this.left = this.right = null;
-        this.Course = course;
-        this.courseList.add(course);
-    }
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+	public Node getListnerBackup1() {
+		return ListnerBackup1;
+	}
+
+	public void setListnerBackup1(Node listnerBackup1) {
+		ListnerBackup1 = listnerBackup1;
+	}
+
+	public Node getListnerBackup2() {
+		return ListnerBackup2;
+	}
+
+	public void setListnerBackup2(Node listnerBackup2) {
+		ListnerBackup2 = listnerBackup2;
+	}
+
+	public String getInsert() {
+		return insert;
+	}
+
+	public void setInsert(String insert) {
+		this.insert = insert;
+	}
+
+	public String getRemove() {
+		return remove;
+	}
+
+	public void setRemove(String remove) {
+		this.remove = remove;
+	}
+
+	public int getOriginalTree() {
+		return originalTree;
+	}
+
+	public void setOriginalTree(int originalTree) {
+		this.originalTree = originalTree;
+	}
+	
+	public Node(Node node)
+	{
+		this.key = node.getKey();
+		this.left = node.getLeft();
+		this.right = node.getRight();
+		this.Course = node.getCourse();
+		this.courseList.add(this.Course);
+	}
+
+	public Node(int bnumber, String course) {
+		this.key = bnumber;
+		this.left = this.right = null;
+		this.Course = course;
+		this.courseList.add(course);
+	}
 
 	@Override
 	public void notifyAll(Student student) {
@@ -58,9 +121,9 @@ public class Node implements ObserverI, SubjectI{
 	@Override
 	public void registerObserver(Node observer) {
 		// TODO Auto-generated method stub
-		ListnerBackup1 = new Node(observer.getKey(),observer.getCourse());
-		ListnerBackup2 = new Node(observer.getKey(),observer.getCourse());
-		
+		ListnerBackup1 = new Node(observer.getKey(), observer.getCourse());
+		ListnerBackup2 = new Node(observer.getKey(), observer.getCourse());
+
 	}
 
 	@Override
@@ -68,8 +131,7 @@ public class Node implements ObserverI, SubjectI{
 		// TODO Auto-generated method stub
 		ListnerBackup1 = null;
 		ListnerBackup2 = null;
-		
-		
+
 	}
 
 	@Override
@@ -77,23 +139,20 @@ public class Node implements ObserverI, SubjectI{
 		// TODO Auto-generated method stub
 		String Course = student.getCourse();
 
-        // Below condition is checked to add the courses
-        if ( student.getOperation().equalsIgnoreCase(insert) && ! courseList.contains(Course) )
-        {
-            this.courseList.add(Course);
-        }
+		// Below condition is checked to add the courses
+		if (student.getOperation().equalsIgnoreCase(insert) && !courseList.contains(Course)) {
+			this.courseList.add(Course);
+		}
 
-        // Below condition is checked to remove the courses
-        if ( student.getOperation().equalsIgnoreCase(remove) && courseList.contains(Course))
-        {
-            this.courseList.remove(Course);
-        }
+		// Below condition is checked to remove the courses
+		if (student.getOperation().equalsIgnoreCase(remove) && courseList.contains(Course)) {
+			this.courseList.remove(Course);
+		}
 
-        // below condition check if it is original tree node then update the backup node
-        if ( this.originalTree == 0 )
-        {
-            this.notifyAll(student);
-        }
-		
-	} 
+		// below condition check if it is original tree node then update the backup node
+		if (this.originalTree == 0) {
+			this.notifyAll(student);
+		}
+
+	}
 }
--- mehul_bhuva_assign_2/studentCoursesBackup/src/studentCoursesBackup/util/TreeBuilder.java
+++ mehul_bhuva_assign_2/studentCoursesBackup/src/studentCoursesBackup/util/TreeBuilder.java
@@ -1,5 +1,106 @@
 package studentCoursesBackup.util;
 
-public class TreeBuilder {
+import java.util.ArrayList;
 
+import studentCoursesBackup.myTree.Node;
+
+public class TreeBuilder {
+	 Node root; 
+	  
+	    // Constructor 
+	    public TreeBuilder() 
+	    { 
+	        root = null; 
+	    } 
+	  
+	    // This method mainly calls deleteRec() 
+	    private void deleteKey(int key) 
+	    { 
+	        root = this.deleteRec(root, key); 
+	    } 
+	  
+	    /* A recursive function to insert a new key in BST */
+	   private Node deleteRec(Node root, int key) 
+	    { 
+	        /* Base Case: If the tree is empty */
+	        if (root == null)  return root; 
+	  
+	        /* Otherwise, recur down the tree */
+	        if (key < root.getKey()) 
+	            root.setLeft(this.deleteRec(root.getLeft(), key)); 
+	        else if (key > root.getKey()) 
+	            root.setRight(this.deleteRec(root.getRight(), key)); 
+	  
+	        // if key is same as root's key, then This is the node 
+	        // to be deleted 
+	        else
+	        { 
+	            // node with only one child or no child 
+	            if (root.getLeft() == null) 
+	                return root.getRight(); 
+	            else if (root.getRight() == null) 
+	                return root.getLeft(); 
+	  
+	         // node with two children: Get the inorder successor (smallest
+	            // in the right subtree)
+	            root.setKey(this.minValue(root.getRight()));
+	            root.setCourseList(this.minValueCourse(root.getRight()));
+	  
+	            // Delete the inorder successor 
+	            root.setRight(this.deleteRec(root.getRight(), root.getKey())); 
+	        } 
+	  
+	        return root; 
+	    } 
+	    
+	    private ArrayList<String> minValueCourse(Node root)
+	    {
+	        int minv = root.getKey();
+	        ArrayList<String> temp = root.getCourseList();
+	        while (root.getLeft() != null)
+	        {
+	            temp = root.getLeft().getCourseList();
+	            minv = root.getLeft().getKey();
+	            root = root.getLeft();
+	        }
+	        return temp;
+	    }
+
+		int minValue(Node root) 
+	    { 
+	        int minv = root.getKey(); 
+	        while (root.getLeft() != null) 
+	        { 
+	            minv = root.getLeft().getKey(); 
+	            root = root.getLeft(); 
+	        } 
+	        return minv; 
+	    } 
+	  
+	  
+	    /* A recursive function to insert a new key in BST */
+	    private void insertRec(Node newNode) 
+	    { 
+	    	root = this.insertRec(root, newNode);
+	    } 
+	    
+	    private Node insertRec(Node root, Node newNode) 
+	    {
+	    	int key = newNode.getKey();
+	        /* If the tree is empty, return a new node */
+	        if (root == null) 
+	        { 
+	            root = new Node(newNode); 
+	            return root; 
+	        } 
+	  
+	        /* Otherwise, recur down the tree */
+	        if (key < root.getKey())
+	            root.setLeft(this.insertRec(root.getLeft(), newNode));
+	        else if (key > root.getKey())
+	            root.setRight(this.insertRec(root.getRight(), newNode));
+	  
+	        /* return the (unchanged) node pointer */
+	        return root; 
+	    }   
 }
